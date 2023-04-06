57726d8e8facecafb177e1514538daef2e18acd9
zia-malik <ziamlaik111222333@gmail.com>
2023-04-06 16:53:05 +0500

    Deployed and verified on etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-etherscan");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat:{},
+    goerli:{
+      url: "your infura url",
+      accounts: ["your private key"]
+    }
+  },
+  etherscan: {
+    apiKey: "Your etherscan api key",
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,22 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const SBT = await hre.ethers.getContractFactory("SBT");
+  const sbt = await SBT.deploy("UniversityOfLahore", "UOL");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await sbt.deployed();
 
-  await lock.deployed();
+  // console.log(
+  //   `NFT contract Deployed to ${nft.address}`
+  // );
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `SBT contract Deployed to ${sbt.address}`
   );
 }
+
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
@@ -30,3 +30,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// SFT deployed to : 0x6314e575516252EF8A798Bd87e28bC17D10b12E9
